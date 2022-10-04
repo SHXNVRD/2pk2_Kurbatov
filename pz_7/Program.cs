f4987a4abaf46de623684414a35a235d2de45934
SHXNVRD <trickmen47@gmail.com>
2022-10-04 13:10:27 +0500

    pz_7

---
+++ pz_7/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace pz_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] myArray = new int[10];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.Write($"{myArray[i] = rnd.Next(-10, 10)} ");
+            }
+            Console.WriteLine();
+            Array.Reverse(myArray, 0, myArray.Length - 1);
+            foreach (int i in myArray) Console.Write(i + " ");
+            Console.ReadKey();
+        }
+    }
+}
