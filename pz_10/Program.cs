6fad4b5cab9a4d1542941fc3c5cf2da93e4a378a
SHXNVRD <trickmen47@gmail.com>
2022-11-14 16:34:22 +0500

    pz_12

--- pz_10/Program.cs
+++ pz_10/Program.cs
@@ -5,37 +5,54 @@
         static void Main(string[] args)
         {
             Console.Write("Введите значение: ");
-            string text = Console.ReadLine();
-
-            /*
-            string[] symbols = text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (string symbol in symbols)
-                Console.Write(symbol + " ");
-            Console.WriteLine();
-
-             var three = char.GetNumericValue('\u0033');
-            Console.WriteLine(three); 
-            Console.WriteLine((int)'\u0033'); 
-            string str1 = str0 ?? "1+2+3";
-            */
-
-            for (int i = 0; i < text.Length; i++)
+            string str = Console.ReadLine();
+            string[] strArray = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] value = new int[3];
+            int i = 0;
+            int action;
+            foreach (string x in strArray)
             {
-                char chr = text[i];
-                int code = (int)chr;
+                string count = x;
+                char[] chr = count.ToCharArray();
+                foreach (char h in chr)
+                {
+                    if (char.IsDigit(h) == true)
+                    {
+                        value[i++] = Convert.ToInt32(h);
+                    }
+                }
             }
 
-            char d = '3';
-            int g = Convert.ToInt32(d);
-            Console.WriteLine(g);
+            switch (str[1])
+            {
+                case '+':
+                    if (str[3] == '+')
+                    {
+                        action = value[0] + value[2] + value[4];
+                        Console.WriteLine(action);
+                    }
+                    else
+                    {
+                        action = value[0] - value[2] - value[4];
+                        Console.WriteLine(action);
+                    }
+                    break;
 
+                    case '-':
+                    if (str[3] == '+')
+                    {
+                        action = value[0] - value[2] + value[4];
+                        Console.WriteLine(action);
+                    }
+                    else
+                    {
+                        action = value[0] - value[2] - value[4];
+                        Console.WriteLine(action);
+                    }
+                    break;
+            }
 
-
-            string s = "ABCD";
-            char ch = s[0];
-            int charnum = (int)ch;
-            Console.WriteLine(charnum);
+            Console.ReadKey();
         }
     }
 }
---
+++ pz_12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace pz_12
+{
+    
+    internal class Program
+    {
+       static void SetValues(out int x, out int y)
+        {
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Sum(int x, int y, out int result)
+        {
+            result = x + y;
+        }
+
+        static void PrintResult(int chislo)
+        {
+            Console.WriteLine(chislo);
+        }
+
+        static void Main(string[] args) 
+        {
+            int x;
+            int y;
+            int result;
+            SetValues(out x, out y);
+            Sum(x, y, out result);
+            PrintResult(result);
+        }
+    }
+}
