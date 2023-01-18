fd12c4c5efa06f84864d03d315eddc9271cdf62b
SHXNVRD <trickmen47@gmail.com>
2023-01-18 20:22:26 +0500

    /

--- pz_20/Program.cs
+++ pz_20/Program.cs
@@ -1,26 +1,28 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Xml.Serialization;
 
 namespace pz_20
 {
     internal class Program
     {
-        static void Name(string text)
+        static public void ReverseRegister(string text)
         {
+            if (text == null) return;
+                
             StringBuilder sb = new StringBuilder(text);
-
+            
             for (int i = 0; i < sb.Length; i++)
             {
-                if (i % 2 != 0)
-                    sb[i] = Char.ToUpper(sb[i]);
-
-            }
-
-            foreach (var str in sb.ToString()) Console.Write(str);
+                if (Char.IsLower(text[i])) sb[i] = Char.ToUpper(sb[i]);
+                else sb[i] = Char.ToLower(sb[i]);
+                Console.Write(sb[i]);
+            }  
         }
         static void Main(string[] args)
         {
-            
+            string str = "HellO WoRRld";
+            ReverseRegister(str);
         }
     }
 }
