6a349bab32d82687513b5f256dbd7476ee24cbd8
SHXNVRD <trickmen47@gmail.com>
2023-01-28 16:48:34 +0500

    \

---
+++ pz_21/DeliveryRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21
+{
+    internal class DeliveryRequest
+    {
+        public int ID { get; set; }
+        public int Summ { get; set; }
+        public string RequestTime { get; set; }
+        public string Adr { get; set; }
+
+        public DeliveryRequest() : this(0)
+        { }
+        public DeliveryRequest(int ID) : this(ID, 0)
+        { }
+        public DeliveryRequest(int ID, int Summ) : this(ID, Summ, "Неизвестно")
+        { }
+        public DeliveryRequest(int ID, int Summ, string RequestTime) : this(ID, Summ, RequestTime, "Неизвестно")
+        { }
+        public DeliveryRequest(int id, int summ, string requestTime, string adr)
+        {
+            ID = id;
+            Summ = summ;
+            RequestTime = requestTime;
+            Adr = adr;
+        }
+
+        public DeliveryRequest(DeliveryRequest deliveryRequest)
+        {
+            ID = deliveryRequest.ID;
+            Summ = deliveryRequest.Summ;
+            RequestTime = deliveryRequest.RequestTime;
+            Adr = deliveryRequest.Adr;
+        }
+        public void GetDeliveryInfo() => Console.WriteLine($"ID: {ID}\nСумма: {Summ}\nВремя заказа: {RequestTime}\nАдресс: {Adr}");
+    }
+}
---
+++ pz_21/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace pz_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DeliveryRequest deliveryRequest = new();
+
+            DeliveryRequest deliveryRequest1 = new()
+            {
+                ID = 21,
+                RequestTime = "16:25"
+            };
+
+            DeliveryRequest deliveryRequest2 = new(deliveryRequest1);
+
+            DeliveryRequest deliveryRequest3 = new(999, 270, "16:27", "Улица Пушкина, дом Колотушкина");
+
+            Console.WriteLine($"{deliveryRequest}\n {deliveryRequest1}\n {deliveryRequest2}\n {deliveryRequest3}\n");
+        }
+    }
+}
